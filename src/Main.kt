79cd723b06cea93a8a3614e936d98bba8ad923db
Admin <reteamboss@gmail.com>
2024-08-26 17:37:01 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+    val array = intArrayOf(1,123,0,32,-1,-234,-43,23,54,-56,-5)
+    println(checkNum(array).toList())
+
+
+}
+
+fun checkNum(array: IntArray): IntArray{
+    for (i in array.indices){
+        if (array[i] <= 0){
+            array[i] = array[i] + 1
+        }
+    }
+    return array
 }
